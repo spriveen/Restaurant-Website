ce2597363e244d9469c448230bababad05f194a6
spriveen <rpeanuka@gmail.com>
2025-08-30 03:28:48 -0700

    update service

---
+++ index.js
@@ -0,0 +1,9 @@
+// navbar scroll
+window.addEventListener('scroll', function () {
+    let navbar = document.querySelector('.navbar');
+    if (window.scrollY > 20) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
