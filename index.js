5cb74506b66a44c9418e802d503011017eb2affa
spriveen <rpeanuka@gmail.com>
2025-08-30 21:47:47 -0700

    update index.js

--- index.js
+++ index.js
@@ -7,3 +7,29 @@ window.addEventListener('scroll', function () {
         navbar.classList.remove('scrolled');
     }
 });
+
+// scroll up
+document.querySelector('#to-top').addEventListener('click', () => {
+  let TopInterval = setInterval(() => {
+    let ArrowTop = document.body.scrollTop > 0 ? document.body : document.documentElement;
+    if (ArrowTop.scrollTop > 0) {
+      ArrowTop.scrollTop = ArrowTop.scrollTop - 50;
+    }
+    if (ArrowTop.scrollTop < 1) {
+      clearInterval(TopInterval);
+    }
+  }, 10);
+}, false);
+
+function showscroll() {
+  let TopButton = document.getElementById('to-top');
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    TopButton.classList.add('show');
+  } else {
+    TopButton.classList.remove('show');
+  }
+}
+
+window.onscroll = () => {
+  showscroll(); // ✅ fixed
+};
